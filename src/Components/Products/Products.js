6d2891931f60c7a357069b901efa7ca56bdffc0d
DESKTOP-UQSIAMQ\User <asfaysal.bracu@gmail.com>
2021-03-31 04:42:49 +0600

    server api link updated

--- src/Components/Products/Products.js
+++ src/Components/Products/Products.js
@@ -5,7 +5,7 @@ const Products = () => {
     const [products, setProducts] = useState([]);
 
     useEffect(() => {
-        fetch("http://localhost:5000/products")
+        fetch("https://daily-groceries-server.herokuapp.com/products")
         .then(res => res.json())
         .then(data => setProducts(data))
     }, [])
