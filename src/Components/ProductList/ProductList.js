2b14ee42f91591e550b03567f427560d1d6f769f
DESKTOP-UQSIAMQ\User <asfaysal.bracu@gmail.com>
2021-03-31 03:15:50 +0600

    Image Added

--- src/Components/ProductList/ProductList.js
+++ src/Components/ProductList/ProductList.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
 const ProductList = (props) => {
-    const { _id, name, price, quantity } = props.product;
+    const { _id, name, price, quantity, imgURL } = props.product;
     return (
         <div>
             <h1>Product Name: {name}</h1>
             <h1>Product Price: {price}</h1>
             <h1>Product Quantity: {quantity}</h1>
+            <img src={imgURL} alt=""/>
             <hr/>
         </div>
     );
