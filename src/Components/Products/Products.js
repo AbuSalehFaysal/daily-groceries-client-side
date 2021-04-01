9f01ce0c161138d8289be780f06a813a3e463b18
DESKTOP-UQSIAMQ\User <asfaysal.bracu@gmail.com>
2021-04-01 18:09:29 +0600

    Spinner added

--- src/Components/Products/Products.js
+++ src/Components/Products/Products.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Spinner } from 'react-bootstrap';
 import ProductList from '../ProductList/ProductList';
 
 const Products = () => {
@@ -6,12 +7,17 @@ const Products = () => {
 
     useEffect(() => {
         fetch("https://secure-hollows-18485.herokuapp.com/products")
-        .then(res => res.json())
-        .then(data => setProducts(data))
+            .then(res => res.json())
+            .then(data => setProducts(data))
     }, [])
     return (
-        <div>
+        <div className="text-center">
             <h1 className="text-center display-1">Welcome to Daily Grocery Shopping</h1>
+            {
+                products.length === 0 && <Spinner animation="border" role="status">
+                    
+                </Spinner>
+            }
             {
                 products.map(product => <ProductList key={product._id} product={product}></ProductList>)
             }
