c0e80c61604d1505fa21ce75ca69780b304d6c0a
DESKTOP-UQSIAMQ\User <asfaysal.bracu@gmail.com>
2021-04-01 21:52:21 +0600

    Frontend Update

--- src/Components/Admin/Admin.js
+++ src/Components/Admin/Admin.js
@@ -2,9 +2,13 @@ import React from 'react';
 
 const Admin = () => {
     return (
-        <div>
+        <div className="container text-center">
             <h1>This is Admin Route!!!!</h1>
             <a href="https://secure-hollows-18485.herokuapp.com" target="_blank">Add Product</a>
+            <br/>
+            <a href="https://secure-hollows-18485.herokuapp.com" target="_blank">Manage Product</a>
+            <br/>
+            <a href="https://secure-hollows-18485.herokuapp.com" target="_blank">Delete Product</a>
         </div>
     );
 };
