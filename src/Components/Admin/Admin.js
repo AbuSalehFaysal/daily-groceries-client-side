790d1bffd3d6249cd3603eac83f939ae8b251f0c
DESKTOP-UQSIAMQ\User <asfaysal.bracu@gmail.com>
2021-04-01 23:33:15 +0600

    UI Update

--- src/Components/Admin/Admin.js
+++ src/Components/Admin/Admin.js
@@ -3,12 +3,14 @@ import React from 'react';
 const Admin = () => {
     return (
         <div className="container text-center">
-            <h1>This is Admin Route!!!!</h1>
-            <a href="https://secure-hollows-18485.herokuapp.com" target="_blank">Add Product</a>
+            <h1 className="display-2">Welcome to Admin Panel</h1>
+            <a className="btn btn-success btn-lg btn-block" href="https://secure-hollows-18485.herokuapp.com" target="_blank">Add Product</a>
             <br/>
-            <a href="https://secure-hollows-18485.herokuapp.com" target="_blank">Manage Product</a>
             <br/>
-            <a href="https://secure-hollows-18485.herokuapp.com" target="_blank">Delete Product</a>
+            <a className="btn btn-primary btn-lg btn-block" href="https://secure-hollows-18485.herokuapp.com" target="_blank">Manage Product</a>
+            <br/>
+            <br/>
+            <a className="btn btn-danger btn-lg btn-block" href="https://secure-hollows-18485.herokuapp.com" target="_blank">Delete Product</a>
         </div>
     );
 };
